e5db83288b980be48e18a2882a7bdb40b66b2a93
azarath1 <ar2oww@gmail.com>
2022-01-19 11:33:23 +0100

    refactor: Login component to send login data for backend

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,51 +1,40 @@
 import './Login.css';
-import {useState} from 'react';
+import { useState } from 'react';
 
 function Login() {
-  // const history = useHistory();
-  const [email, setEmail] = useState("Name");
+  const [email, setEmail] = useState("Email");
   const [password, setPassword] = useState("Password");
 
   async function handleSubmit(event) {
-      event.preventDefault();
-      await fetch( 'http://localhost:4000/login', {
-          method: 'post',  
-          mode:'cors',
-          headers : { 
-              'Access-Control-Allow-Credentials':'true',
-              'Content-Type': 'application/json',
-              'Accept': 'application/json'
-          },
-          body: JSON.stringify({
-              "username": email,
-              "password": password
-          }),
-      })
-          .then(res => console.log(res));
-          // .then((res) => {
-          //     if(res.error){
-          //         alert(res.error)
-          //     }
-          //     console.log(res);
-          //     // setWithExpiry(res);
-          //     // history.push("/");
-          //     // setTimeout(() => {
-          //     //     window.location.reload();    
-          //     //   }, 500);
-          // })
+    event.preventDefault();
+    await fetch('http://localhost:4000/login', {
+      method: 'post',
+      mode: 'cors',
+      headers: {
+        'Access-Control-Allow-Credentials': 'true',
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({
+        "email": email,
+        "password": password
+      }),
+    })
+      .then(res => res.json())
+      .then(console.log);
   }
 
   return (
     <div className="Login">
-      	<div className="container">
-		      <h1>Welcome</h1>
-		
-          <form className="form" onSubmit={handleSubmit}>
-            <input name="email" type="text" placeholder="Email"  onChange={event => setEmail(event.target.value)}></input>
-            <input name="password" type="password" placeholder="Password"  onChange={event => setPassword(event.target.value)}></input>
-            <button type="submit" id="login-button">Login</button>
-          </form>
-	      </div>
+      <div className="container">
+        <h1>Welcome</h1>
+
+        <form className="form" onSubmit={handleSubmit}>
+          <input name="email" type="text" placeholder="Email" onChange={event => setEmail(event.target.value)}></input>
+          <input name="password" type="password" placeholder="Password" onChange={event => setPassword(event.target.value)}></input>
+          <button type="submit" id="login-button">Login</button>
+        </form>
+      </div>
     </div>
   );
 }
