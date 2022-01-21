df73d8d9c3b18033cc11ed982998d7cc8e24ec3e
azarath1 <ar2oww@gmail.com>
2022-01-21 11:05:32 +0100

    feat: add basic user page

---
+++ client/src/components/UserPage.js
@@ -0,0 +1,16 @@
+function UserPage() {
+  
+  return (
+    <div className="Userpage">
+      <div className="container">
+        <h1>Welcome <span>Géza</span>!</h1>
+        <p>Userid: 1</p>
+        <p>Username: Géza</p>
+        <p>Email: geza@geza.com</p>
+        <button type="submit">Logout</button>
+      </div>
+    </div>
+  );
+}
+
+export default UserPage;
