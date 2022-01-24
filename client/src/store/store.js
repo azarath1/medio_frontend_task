60b74b5453754d319b08de3ef71d1beccc0acc62
azarath1 <ar2oww@gmail.com>
2022-01-24 13:06:54 +0100

    feat: implement user persistence layer for redux store

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,8 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from '../features/userSlice';
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+    key: 'main-root',
+    storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, userReducer);
 
 export default configureStore({
     reducer: {
-        user: userReducer,
+        user: persistedReducer,
     },
 });
