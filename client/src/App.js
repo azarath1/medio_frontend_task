0126bdb39aa311b89ec9c013f3033ae6a36cb40d
azarath1 <ar2oww@gmail.com>
2022-01-24 13:08:53 +0100

    feat: wrap app component with persistor gate

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ const App = () => {
 
     return (
         <div>
-           {user ? <UserPage/> : <Login/>}
+            {user ? <UserPage /> : <Login />}
         </div>
     );
 };
--- client/src/index.js
+++ client/src/index.js
@@ -5,12 +5,17 @@ import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './store/store.js';
 import App from './App';
+import { PersistGate } from 'redux-persist/integration/react';
+import persistStore from 'redux-persist/lib/persistStore';
 
+const Persistor = persistStore(store);
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store = {store}>
-      <App/>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={Persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
