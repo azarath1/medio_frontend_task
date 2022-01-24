50e1ec7490139bf51112d41adf70dc9d883550fa
azarath1 <ar2oww@gmail.com>
2022-01-24 08:38:07 +0100

    feat: add UserPage details

--- client/src/components/UserPage.js
+++ client/src/components/UserPage.js
@@ -1,13 +1,24 @@
-function UserPage() {
-  
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { logout, selectUser } from '../features/userSlice';
+
+const UserPage = () => {
+  const dispatch = useDispatch();
+  const user = useSelector(selectUser);
+
+  const handleLogout = (event) => {
+    event.preventDefault();
+    dispatch(logout());
+  }
+
   return (
     <div className="Userpage">
       <div className="container">
-        <h1>Welcome <span>Géza</span>!</h1>
-        <p>Userid: 1</p>
-        <p>Username: Géza</p>
-        <p>Email: geza@geza.com</p>
-        <button type="submit">Logout</button>
+        <h1>Welcome <span>{user.userName}</span>!</h1>
+        <p>Username: {user.userName}</p>
+        <p>Email: {user.email}</p>
+        <p>Online since: <span>{1}</span> days</p>
+        <button onClick={(event) => handleLogout(event)}>Logout</button>
       </div>
     </div>
   );
