11183d2e0f8a78e0aa26084b65fe5d6716eabb3f
azarath1 <ar2oww@gmail.com>
2022-01-25 13:45:54 +0100

    feat: add cityViewer function to user page

--- client/src/components/UserPage.js
+++ client/src/components/UserPage.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { logout, selectUser } from '../features/userSlice';
 import moment from 'moment';
+import './Login.css';
+import { cityview } from '../features/userSlice';
 
 const UserPage = () => {
   const dispatch = useDispatch();
@@ -13,16 +15,22 @@ const UserPage = () => {
   }
 
   return (
-    <div className="Userpage">
-      <div className="container">
+    <div className="Login">
+      <button className='cities' onClick={cityViewer}>Városok</button>
+      <div className="formcontainer">
         <h1>Welcome <span>{user.userName}</span>!</h1>
         <p>Username: {user.userName}</p>
         <p>Email: {user.email}</p>
         <p id="online">Online since: <span>{moment().diff(user.onlineSince, 'days')} days</span></p>{/* checking online presence for days */}
-        <button onClick={(event) => handleLogout(event)}>Logout</button>
+        <button onClick={(event) => handleLogout(event)}>Kilépés</button>
       </div>
     </div>
   );
+
+  function cityViewer() {
+    dispatch(cityview());
+  }
+
 }
 
 export default UserPage;
