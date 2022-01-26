d6a62db9b86b84dac23b71f3d5652a7f2ac7f7ae
azarath1 <ar2oww@gmail.com>
2022-01-26 16:03:56 +0100

    feat: add navigating hook based on redux site pointer state

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector } from "react-redux";
 import Cities from './components/Cities';
 import Login from "./components/Login";
 import UserPage from './components/UserPage';
-import { selectUser } from "./features/userSlice";
+import { selectSite } from "./features/siteInfo";
+import { Routes, Route } from 'react-router-dom';
+import ProtectedRoutes from './features/ProtectedRoutes';
+import { useNavigate } from 'react-router-dom';
+import { useSearchParams } from 'react-router-dom';
 
 const App = () => {
-    const user = useSelector(selectUser);
+    const currentSite = useSelector(selectSite);
+    const navigate = useNavigate();
+    const [searchParams] = useSearchParams();
+    const searchMegye = searchParams.get('megye') || '';
+
+    useEffect(() => {
+        if (currentSite === null || currentSite === "logout") {
+            navigate("/");
+        }
+        else if (currentSite === "userpage") {
+            navigate("/userpage");
+        }
+        else if (currentSite === "city") {
+            navigate("/cities");
+        }
+        else if (currentSite === "query") {
+            navigate("/cities?megye=" + searchMegye);
+        }
+
+    }, [navigate, currentSite, searchMegye]);
+
     return (
         <Routes>
             <Route path="/" element={<Login />} />
