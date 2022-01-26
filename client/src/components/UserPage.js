0f0c8175cb36a21c7e2a8ffe2afc7f2751489675
azarath1 <ar2oww@gmail.com>
2022-01-26 16:07:30 +0100

    refactor: user page now using router

--- client/src/components/UserPage.js
+++ client/src/components/UserPage.js
@@ -3,7 +3,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import { logout, selectUser } from '../features/userSlice';
 import moment from 'moment';
 import './Login.css';
-import { cityview } from '../features/userSlice';
+import { cityview, logoutSite } from '../features/siteInfo';
+import { Link } from 'react-router-dom';
 
 const UserPage = () => {
   const dispatch = useDispatch();
@@ -12,17 +13,18 @@ const UserPage = () => {
   const handleLogout = (event) => {
     event.preventDefault();
     dispatch(logout());
+    dispatch(logoutSite());
   }
 
   return (
     <div className="Login">
-      <button className='cities' onClick={cityViewer}>Városok</button>
+      <Link onClick={cityViewer} to="/cities"><button>Városok</button></Link>
       <div className="formcontainer">
         <h1>Welcome <span>{user.userName}</span>!</h1>
         <p>Username: {user.userName}</p>
         <p>Email: {user.email}</p>
         <p id="online">Online since: <span>{moment().diff(user.onlineSince, 'days')} days</span></p>{/* checking online presence for days */}
-        <button onClick={(event) => handleLogout(event)}>Kilépés</button>
+        <Link to="/" onClick={(event) => handleLogout(event)}><button>Kilépés</button></Link>
       </div>
     </div>
   );
